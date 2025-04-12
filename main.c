33ead440423fb25a307f00de321f1a21d6497c04
Matias <matiasmonteiroaraujo@gmail.com>
2025-04-12 15:01:38 -0300

    Desenvolvendo a lógica do main

--- main.c
+++ main.c
@@ -3,16 +3,27 @@
 
 int main(){
 
-    int escolha;
+    int escolha, identif;
     Colecao dados;
 
+    do{
     printf("Bem-vindo!");
-    printf("\nO que deseja fazer?");
-    printf("\n1. Cadastrar item");
+    printf("\n1. Cadastrar item.");
+    printf("\n2. Consultar item.");
+    printf("\n0. Sair.");
+    printf("\nO que deseja fazer? ");
     scanf("%d", &escolha);
 
     if(escolha == 1){
         dados = cadastrar_item();
         gerar_arquivo(dados);
     }
+    if (escolha == 2){
+        printf("Digite o identificador do item: ");
+        scanf("%d", &identif);
+
+        consultar_item("colecao.txt", identif);
+    }
+}while(escolha != 0);
+    
 }
